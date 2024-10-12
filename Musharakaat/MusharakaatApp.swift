f2f22acf8e275f49c8e316cb0a97546846d0c6f5
Owais Quadri <owaisquadri01@gmail.com>
2024-10-12 08:21:42 -0400

    Initial Commit

---
+++ Musharakaat/MusharakaatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusharakaatApp.swift
+//  Musharakaat
+//
+//  Created by Owais on 2024-10-12.
+//
+
+import SwiftUI
+
+@main
+struct MusharakaatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusharakaatTests/MusharakaatTests.swift
@@ -0,0 +1,16 @@
+//
+//  MusharakaatTests.swift
+//  MusharakaatTests
+//
+//  Created by Owais on 2024-10-12.
+//
+
+import Testing
+
+struct MusharakaatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
